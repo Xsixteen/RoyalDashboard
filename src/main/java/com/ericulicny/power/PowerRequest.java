9482b624d9294d578687a4504cfb8813c851b60c
Eric <x16.tux@gmail.com>
2018-05-06 22:03:55 -0400

    Updates for a new page that will display monthly temperatures.

---
+++ src/main/java/com/ericulicny/power/PowerRequest.java
@@ -0,0 +1,26 @@
+package com.ericulicny.power;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class PowerRequest {
+    
+    public static String getPowerKW(String urlString) throws Exception {
+        
+        StringBuilder result = new StringBuilder();
+        URL url = new URL(urlString);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line = rd.readLine()) != null) {
+           result.append(line);
+        }
+        rd.close();
+        String stringResult = result.toString();
+        return stringResult.substring(0, stringResult.length()-2).trim();    
+    }
+}
---
+++ src/main/java/com/ericulicny/power/PowerUtilizationConstants.java
@@ -0,0 +1,5 @@
+package com.ericulicny.power;
+
+public class PowerUtilizationConstants {
+    public final static String CONST_POWERURL = "/instantaneousdemand"; 
+}
