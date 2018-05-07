91eb8bbbab4d7504912b0b815d2b5c6f9450079a
Eric <x16.tux@gmail.com>
2018-05-06 21:48:10 -0400

    Updates for a new page that will display monthly temperatures.

---
+++ src/main/java/com/eulicny/homedashboard/dao/PowerUtilization.java
@@ -0,0 +1,39 @@
+package com.eulicny.homedashboard.dao;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "PowerUtilization")
+@Entity
+public class PowerUtilization {
+    Double powerKW;
+    
+    @Id
+    private Long epochTime;
+
+
+
+    public Double getPowerKW() {
+        return powerKW;
+    }
+
+    public void setPowerKW(Double powerKW) {
+        this.powerKW = powerKW;
+    }
+
+    public Long getEpochTime() {
+        return epochTime;
+    }
+
+    public void setEpochTime(Long epochTime) {
+        this.epochTime = epochTime;
+    }
+    
+    @Override
+    public String toString() {
+        return String.format(
+                "TempHumidity[epoch=%s, Power='%s]",
+                epochTime, powerKW);
+    }
+}
