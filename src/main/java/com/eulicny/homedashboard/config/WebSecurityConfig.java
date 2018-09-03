761474eeffe7fbd167b490270c941eff20164578
Eric <x16.tux@gmail.com>
2018-09-03 18:37:49 -0400

    Spring Boot Security Testing

--- src/main/java/com/eulicny/homedashboard/config/WebSecurityConfig.java
+++ src/main/java/com/eulicny/homedashboard/config/WebSecurityConfig.java
@@ -4,23 +4,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/api/**").permitAll()
                 .antMatchers("/**").hasRole("USER").anyRequest().authenticated()
                 .and().csrf().disable()
                 .formLogin()
                 .loginPage("/login.html").defaultSuccessUrl("/index.html")
                 .permitAll();
+
+
+    }
+
+    public void configure(WebSecurity web) throws Exception {
+
+        web.ignoring().antMatchers(
+                "/api/**"
+                );
+
     }
 
     @Autowired
@@ -29,4 +38,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .inMemoryAuthentication()
                 .withUser("user").password("password").roles("USER");
     }
+
 }
