fb830c7f682be692e607d22922864af330840eef
Eric <x16.tux@gmail.com>
2018-09-03 18:14:16 -0400

    Spring Boot Security Testing

--- src/main/java/com/eulicny/homedashboard/config/WebSecurityConfig.java
+++ src/main/java/com/eulicny/homedashboard/config/WebSecurityConfig.java
@@ -7,16 +7,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-//@Configuration
-//@EnableWebSecurity
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/api/login").permitAll()
-                .antMatchers("/api/network/**").permitAll()
+                .antMatchers("/api/**").permitAll()
                 .antMatchers("/**").hasRole("USER").anyRequest().authenticated()
                 .and().csrf().disable()
                 .formLogin()
