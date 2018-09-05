dd66a2bc501949e1913fe6ed23193d65151769b6
Eric <x16.tux@gmail.com>
2018-09-04 22:09:48 -0400

    Integrating Camera features

--- src/main/java/com/ericulicny/homedashboard/rest/LoginController.java
+++ src/main/java/com/ericulicny/homedashboard/rest/LoginController.java
@@ -1,10 +1,5 @@
 package com.ericulicny.homedashboard.rest;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,20 +7,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
+    //@Autowired
+    //private AuthenticationManager authenticationManager;
 
     @RequestMapping("/api/login")
     public String login(@RequestParam(value="username") String name, @RequestParam(value="password") String password) {
         System.out.println("My login controller");
         System.out.println("User name is " + name);
         System.out.println("Passwird is " + password);
-        Authentication request = new UsernamePasswordAuthenticationToken(name, password);
-
-        Authentication result = authenticationManager.authenticate(request);
-        System.out.println(result.getPrincipal());
-        System.out.println(result.getAuthorities());
-        SecurityContextHolder.getContext().setAuthentication(result);
+    //    Authentication request = new UsernamePasswordAuthenticationToken(name, password);
+//
+      //  Authentication result = authenticationManager.authenticate(request);
+      //  System.out.println(result.getPrincipal());
+      //  System.out.println(result.getAuthorities());
+      //  SecurityContextHolder.getContext().setAuthentication(result);
         return "redirect:/index.html";
     }
 
