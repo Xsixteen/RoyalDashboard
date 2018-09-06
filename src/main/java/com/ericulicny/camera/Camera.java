0edc6eb773c268b7defc0d2ef3e18c874d015243
Eric <x16.tux@gmail.com>
2018-09-05 22:40:13 -0400

    Enabling Camera

--- src/main/java/com/ericulicny/camera/Camera.java
+++ src/main/java/com/ericulicny/camera/Camera.java
@@ -1,18 +1,23 @@
 package com.ericulicny.camera;
 
+import com.ericulicny.homedashboard.rest.CameraController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Camera {
 
     private String rootPath;
     private final static String CONST_SNAPCOMMAND = "raspistill -o  ";
+    private static final Logger log = LoggerFactory.getLogger(Camera.class);
 
     public Camera(String rootPath) {
         this.rootPath = rootPath;
     }
 
     public boolean snapPicture() {
-
-        ProcessBuilder processBuilder = new ProcessBuilder(CONST_SNAPCOMMAND + this.rootPath + "/" + "current.jpg");
-
+        String command = CONST_SNAPCOMMAND + this.rootPath + "/" + "current.jpg";
+        ProcessBuilder processBuilder = new ProcessBuilder(CONST_SNAPCOMMAND, this.rootPath+"/"+"current.jpg");
+        log.info("Snapping picture by issuing command: " + command);
 // 		System.out.println("Executed this command:\n\t" + command.toString());
 // 		pb.redirectErrorStream(true);
 // 		pb.redirectOutput(
